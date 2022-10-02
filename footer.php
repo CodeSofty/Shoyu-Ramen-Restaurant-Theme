9db3f52712e39cec6e2ebf596afe8a39db3fc4ec
CodeSoftly <eclittlejohn@yahoo.com>
2022-10-01 23:34:38 -0500

    commit

--- footer.php
+++ footer.php
@@ -1,1 +1,13 @@
 <!-- Footer here --> 
+
+<Footer>
+    <div class='col-1'>
+    COL 1 
+    </div>
+    <div class='col-1'>
+        COL 2
+        </div>
+        <div class='col-1'>
+        COL 3
+        </div>
+</Footer>
