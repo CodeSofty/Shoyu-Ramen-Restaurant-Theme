0388f65c51577fbd86bc783bd8f377a4409ff6a4
CodeSoftly <eclittlejohn@yahoo.com>
2022-10-20 01:10:06 -0500

    cleaned up about section

--- front-page.php
+++ front-page.php
@@ -2,34 +2,40 @@
     <?php get_header(); ?>
     <section id="about-section">
         <div class="about-left-side flex-row col-reverse">
-            <picture>
-                <source srcset="<?php echo get_template_directory_uri();?>/images/about-restaurant-large.jpg" alt="interior picture of a nice restaurant" media="(min-width: 650px)">
-                <source srcset="<?php echo get_template_directory_uri();?>/images/about-restaurant-small.jpg" alt="interior picture of a nice restaurant" media="(min-width: 300px)">
-                <img src="<?php echo get_template_directory_uri();?>/images/about-restaurant-small.jpg" alt="interior picture of a nice restaurant">
-            </picture>
+            <div class='image-container'>
+                <picture>
+                    <source srcset="<?php echo get_template_directory_uri();?>/images/about-restaurant-large.jpg" alt="interior picture of a nice restaurant" media="(min-width: 650px)">
+                    <source srcset="<?php echo get_template_directory_uri();?>/images/about-restaurant-small.jpg" alt="interior picture of a nice restaurant" media="(min-width: 300px)">
+                    <img src="<?php echo get_template_directory_uri();?>/images/about-restaurant-small.jpg" alt="interior picture of a nice restaurant">
+                </picture>
+            </div>
             <div class="about-text flex-col">
                 <h2 class="text-neutral-900 fs-secondary-heading fw-regular">Our Passion</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Quae eum rerum maiores labore pariatur maxime 
-                    adipisci eaque ipsam quaerat assumenda accusamus rem,
-                    nobis hic aperiam esse facilis laudantium dolor atque!
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida felis at tellus tempus convallis 
+                    maximus a dui. Fusce porta urna vitae ullamcorper facilisis. Fusce ipsum felis, tempus quis cursus eleifend, 
+                    accumsan non sem. In auctor ac leo in imperdiet. Etiam rutrum, dolor quis porta dignissim, sapien leo porta 
+                    tortor, in viverra nibh purus quis enim. Fusce varius eleifend iaculis. Nullam pulvinar libero eros, ac 
+                    scelerisque eros sollicitudin eget.
                 </p>
             </div>
         </div>
         <div class="about-right-side flex-row">
             <div class=" about-text flex-col">
                     <h2 class="text-neutral-900 fs-secondary-heading fw-regular">Our Promise</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Quae eum rerum maiores labore pariatur maxime 
-                        adipisci eaque ipsam quaerat assumenda accusamus rem,
-                        nobis hic aperiam esse facilis laudantium dolor atque!
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida felis at tellus tempus convallis 
+                        maximus a dui. Fusce porta urna vitae ullamcorper facilisis. Fusce ipsum felis, tempus quis cursus eleifend, 
+                        accumsan non sem. In auctor ac leo in imperdiet. Etiam rutrum, dolor quis porta dignissim, sapien leo porta 
+                        tortor, in viverra nibh purus quis enim. Fusce varius eleifend iaculis. Nullam pulvinar libero eros, ac 
+                        scelerisque eros sollicitudin eget.
                     </p>
                 </div>
-                <picture>
-                <source srcset="<?php echo get_template_directory_uri(); ?>/images/about-cooking-large.jpg" alt="a close up of cooking noodles in a pan" media="(min-width: 650px)">
-                    <source srcset="<?php echo get_template_directory_uri(); ?>/images/about-cooking-small.jpg" alt="a close up of cooking noodles in a pan" media="(min-width: 300px)">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/about-cooking-small.jpg" alt="a close up of cooking noodles in a pan">
-                </picture>
+                <div class='image-container'>
+                    <picture>
+                    <source srcset="<?php echo get_template_directory_uri(); ?>/images/about-cooking-large.jpg" alt="a close up of cooking noodles in a pan" media="(min-width: 650px)">
+                        <source srcset="<?php echo get_template_directory_uri(); ?>/images/about-cooking-small.jpg" alt="a close up of cooking noodles in a pan" media="(min-width: 300px)">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/about-cooking-small.jpg" alt="a close up of cooking noodles in a pan">
+                    </picture>
+                </div>
             </div>
     </section>
     <section id="menu-section">
