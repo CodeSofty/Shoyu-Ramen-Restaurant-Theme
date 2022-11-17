5e6b7f39bce83ddbd040e9d2ad9523b68dc71863
CodeSoftly <eclittlejohn@yahoo.com>
2022-11-16 22:48:14 -0600

    mobile nav toggle

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 // Mobile nav funciton
 
 const mobileSvg = document.getElementById('mobile_svg');
+const mobileNav = document.getElementsByClassName('mobile-nav')[0];
+const mobileCloseOut = document.getElementById('close-out');
+
+
 mobileSvg.addEventListener('click', () => {
     toggle_display();
 });
 
-const mobileCloseOut = document.getElementById('close-out');
 
 mobileCloseOut.addEventListener('click', () => {
     toggle_display();
 });
 
+function toggle_display() {
+    mobileNav.classList.toggle('active');
+};
