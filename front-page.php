57af588decb67d8d7ae98046803377d2090294e5
CodeSoftly <eclittlejohn@yahoo.com>
2022-12-08 23:53:58 -0600

    commit

--- front-page.php
+++ front-page.php
@@ -1,43 +1,51 @@
 <!-- About -->
     <?php get_header(); ?>
+
+
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+            $about = get_field('about', 104);
+            $firstRow = $about['first_row'];  ?>
+
     <section id="about-section">
         <div class="about-left-side flex-row col-reverse">
             <div class='image-container'>
-                <picture>
-                    <source srcset="<?php  the_post_thumbnail('about-image')?>" alt="interior picture of a nice restaurant" media="(min-width: 650px)">
-                    <source srcset="<?php  the_post_thumbnail('about-image')?>" alt="interior picture of a nice restaurant" media="(min-width: 300px)">
-                    <img src="<?php  the_post_thumbnail('about-image')?>" alt="interior picture of a nice restaurant">
-                </picture>
+                <img src="<?php $firstRow['first_row_image']['url']?>">
+                <!-- <picture>
+                    <source srcset="" alt="interior picture of a nice restaurant" media="(min-width: 650px)">
+                    <source srcset="" alt="interior picture of a nice restaurant" media="(min-width: 300px)">
+                    <img src="" alt="interior picture of a nice restaurant">
+                </picture> -->
             </div>
             <div class="about-text flex-col">
-                <h2 class="about-heading text-neutral-900 fs-secondary-heading fw-regular">Our Passion</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida felis at tellus tempus convallis 
-                    maximus a dui. Fusce porta urna vitae ullamcorper facilisis. Fusce ipsum felis, tempus quis cursus eleifend, 
-                    accumsan non sem. In auctor ac leo in imperdiet. Etiam rutrum, dolor quis porta dignissim, sapien leo porta 
-                    tortor, in viverra nibh purus quis enim. Fusce varius eleifend iaculis. Nullam pulvinar libero eros, ac 
-                    scelerisque eros sollicitudin eget.
+                <h2 class="about-heading text-neutral-900 fs-secondary-heading fw-regular"><?php $firstRowHeading = $about['first_row']['heading']; echo $firstRowHeading ?></h2>
+                <p><?php $firstRowText = $about['first_row']['first_row_text']; echo $firstRowText ?>
                 </p>
             </div>
         </div>
         <div class="about-right-side flex-row">
             <div class=" about-text flex-col">
-                    <h2 class="about-heading text-neutral-900 fs-secondary-heading fw-regular">Our Promise</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida felis at tellus tempus convallis 
-                        maximus a dui. Fusce porta urna vitae ullamcorper facilisis. Fusce ipsum felis, tempus quis cursus eleifend, 
-                        accumsan non sem. In auctor ac leo in imperdiet. Etiam rutrum, dolor quis porta dignissim, sapien leo porta 
-                        tortor, in viverra nibh purus quis enim. Fusce varius eleifend iaculis. Nullam pulvinar libero eros, ac 
-                        scelerisque eros sollicitudin eget.
+                    <h2 class="about-heading text-neutral-900 fs-secondary-heading fw-regular"><?php $secondRowHeading = $about['second_row']['heading']; echo $secondRowHeading ?></h2>
+                    <p><?php $secondRowText = $about['second_row']['second_row_text']; echo $secondRowText ?>
                     </p>
                 </div>
                 <div class='image-container'>
-                    <picture>
-                    <source srcset="<?php  the_post_thumbnail('about-image')?>" alt="a close up of cooking noodles in a pan" media="(min-width: 650px)">
+                    <img src="<?php $secondRow['second_row_image']['url']?>">
+                    <!-- <picture> -->
+                    <!-- <source srcset="<?php  the_post_thumbnail('about-image')?>" alt="a close up of cooking noodles in a pan" media="(min-width: 650px)">
                         <source srcset="<?php  the_post_thumbnail('about-image')?>" alt="a close up of cooking noodles in a pan" media="(min-width: 300px)">
-                        <img src="<?php  the_post_thumbnail('about-image')?>" alt="a close up of cooking noodles in a pan">
-                    </picture>
+                        <img src="<?php  the_post_thumbnail('about-image')?>" alt="a close up of cooking noodles in a pan"> -->
+                    <!-- </picture> -->
                 </div>
             </div>
     </section>
+
+<?php } // end while
+} // end if
+?>
+
 
 <!-- Menu -->
     <section id="menu-section">
